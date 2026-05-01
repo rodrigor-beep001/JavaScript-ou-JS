c4d643ccea7e501ff01578b062eb6ebd2ae725c4
rodrigor-beep001 <rodrigor.filho13@gmail.com>
2026-05-01 13:09:39 -0300

    Update tags.js

--- Brincaderas s Guanabara/tags.js
+++ Brincaderas s Guanabara/tags.js
@@ -1,17 +1,25 @@
 // 1. No clique do botão pegar o valor de todos os campos;
-// 2. Validar mínimo e máximo de carácteres do campo Nome
-// 3. Validar mínimo máximno e presença de @, e .? no E-mail
-// 4..Validar mínimo e máximo de carácteres e presença de carácteres especiais da Senha
-// 5. Validar mínimo de 18 anos em Data
-// 6. Colocar Gênero como opcional 
+// 2. Validar mínimo e máximo de carácteres do campo Nome;
+// 3. Validar mínimo máximno e presença de @, e .? no E-mail;
+// 4..Validar mínimo e máximo de carácteres e presença de carácteres especiais da Senha;
+// 5. Validar mínimo de 18 anos em Data;
+// 6. Colocar Gênero como opcional
 function validateAndSend() {
-  const emailInput = document.querySelector('input[type=email]')
-  const passwordInput = document.querySelector('input[type=password]')
-  console.log(emailInput.value, passwordInput.value)
+  const nameValue = document.querySelector('input[type=text]').value
+  const emailValue = document.querySelector('input[type=email]').value
+  const passwordValue = document.querySelector('input[type=password]').value
+  const birthdayValue = new Date(document.querySelector('input[type=date]').value)
+  const maleValue = document.querySelector('input#male').checked
+  const femaleValue = document.querySelector('input#female').checked
+
+  console.log(birthdayValue.getFullYear(), new Date().getFullYear)
 
   // pode-se usar e/ou para comparar 2 ou mais coisas, usando: "&&" "||"
 
-  if (emailInput.value.length < 5 || passwordInput.value.length < 8) {
-    console.log('Mínimo de letras não atingido ')
+  if (nameValue.length < 3 || nameValue.length > 255) {
+    console.log('Quantidade de caractéres do nome incorreta :(')
+  } else if (emailValue.length < 6 || emailValue.length > 255) { console.log('Quantidade de caractéres do email incorreta :(') } else if (passwordValue.length > 8 || passwordValue.length > 255) { 'Quantidade de caractéres do senha incorreta :(' } else {
+    console.log(nameValue, emailValue, passwordValue, birthdayValue, maleValue, femaleValue)
+    console.log('Seu cadastro foi um  sucesso!!! :)')
   }
 }
